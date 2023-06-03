9393a4f4287bc4d2f7fbe61e55cd902e0c3b25a1
unknown <creativeapurba@gmail.com>
2023-06-03 23:53:08 +0530

    API added

---
+++ backend.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const body_parser = require('body-parser');
+const https = require('node:https');
+const api_id = "0581781fdefc3d7b7e63f800e43d2804";
+const app = express();
+app.use(body_parser.urlencoded({extended:true}));
+const port = 4000;
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname +'/index.html')
+
+});
+app.post('/',(req,res)=>{
+  const searchString = req.body.city_name;
+  let url = "https://api.openweathermap.org/data/2.5/weather?q=LONDON&units=metric&appid=0581781fdefc3d7b7e63f800e43d2804";
+  https.get(url,(response)=>{
+    console.log(response.statusCode);
+  })
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=${api_id}&units=metric`
+  // https.get(url, (response)=>{
+  //   response.statusCode();
+  // })
+  res.send(searchString);
+});  
+    
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
+// Build an express app which will take CITY NAME from the client side 
+// and search the WEATHER for the concern CITY NAME.(Hint: Use openweather api, body-parser package)
