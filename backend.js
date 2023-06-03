750712142b12de33e5980c575dc471520c18d313
unknown <creativeapurba@gmail.com>
2023-06-04 00:02:59 +0530

    Add Weather by City

--- backend.js
+++ backend.js
@@ -12,16 +12,29 @@ app.get('/',(req,res)=>{
 });
 app.post('/',(req,res)=>{
   const searchString = req.body.city_name;
-  let url = "https://api.openweathermap.org/data/2.5/weather?q=LONDON&units=metric&appid=0581781fdefc3d7b7e63f800e43d2804";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=${api_id}&units=metric`
   https.get(url,(response)=>{
     console.log(response.statusCode);
+    response.on('data', (data) => {
+        const weather = JSON.parse(data);
+        console.log(weather);
+        const temp = weather.main.temp;
+        const city = weather.name;
+        const weatherCondition = weather.weather[0].main
+        const hum = weather.main.humidity;
+
+        weatherData = {
+            temperature:temp,
+            cityName:city,
+            weatherStatus: weatherCondition,
+            humidity:hum
+        }
+
+        res.send(weatherData);
+      });
   })
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=${api_id}&units=metric`
-  // https.get(url, (response)=>{
-  //   response.statusCode();
-  // })
-  res.send(searchString);
-});  
+});   
     
 
 
